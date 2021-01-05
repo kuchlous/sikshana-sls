f263ca6825f2f27fffbfc21b46e665b0f6fde86e
Alok Kuchlous <alokk@mirafra.com>
2021-01-05 22:34:22 +0530

    Use puppeteer instead of wkhtml2pdf

--- create_images.py
+++ create_images.py
@@ -1,6 +1,4 @@
 import os
-# txt = "she takes us on lots of field trips in the Magic school bus. Believe me, it's not called magic for nothing. We never know what's going to happen when we get on that bus."
-#txt = "There is a fail at the local dispute ground to friends. Jr and I are planning to go to the failed to buy Some Thais love each other. Did you hear about the door, Fay? No, There is a located. It is happening at our local district Ground. It is a very beautiful Phil. Really. We should go. Then I will ask Averted friends to We can all go on by Lords of toys for ourselves. I'm so excited. Jail me Dove e j. After the field, J R and B J came back home and displayed their ties. Why did you get for yourself, BJ? I bought the more control card for myself and the teddy bear for my little brother. What did you get? J Im bought a pair of top choice words for my sister on myself. We will play with this war Like that show in the back gardens. You love everything we bought journal. It's Colisee things We will all play together"
 txt = "This is an amazing short story for kids, the ugly duckling story. Once upon a time, there was a duck. She lived in the forest. One day she laid some eggs. After warming them carefully, the duck waited for the eggs to hatch. As she watched, three of her eggs cracked and three lovely ducklings came into the world."
 words = txt.split(' ')
 prefix = ""
@@ -11,5 +9,5 @@ for i in range(len(words)):
     html = prefix + ' ' + highlighted_word + ' ' + suffix
     with open("tmp.html", "w") as f:
         f.write(html)
-    cmd = "wkhtmltoimage tmp.html images-sikshana/" + str(i) + ".jpg"
+        cmd = "node puppet.js file://{0}/tmp.html images-sikshana/{1}.jpg".format(os.getcwd(), str(i))
     os.system(cmd)
